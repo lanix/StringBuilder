f7d32bdd4e527168b607d66530075f643b066fbd
Filiberto Cota <filiberto.cota@gmail.com>
2013-03-21 17:07:53 -0700

    Refactoring
    Signed-off-by: Filiberto Cota <filiberto.cota@gmail.com>

--- js/StringBuilder.js
+++ js/StringBuilder.js
@@ -1,10 +1,22 @@
 (function(box){
 
+
+    var isArray = null;
+
+    if(Array.isArray) {
+        isArray = Array.isArray;
+    }
+    else { 
+        isArray = function (value) {
+            return Object.prototype.toString.apply(value) === '[object Array]';
+        } 
+    }
+
     var StringBuilder = function(){
         var that = this === box ? new StringBuilder() : this;
         that.buffer = [];
-        that.myStack = [];
-        that.myStackBackup = [];
+        that.wrappingStack = [];
+        that.wrappingStackBackup = [];
         that.wrapping = false;
 
         return that;
@@ -15,48 +27,48 @@
             var currentArgument = null;
             var prefix = null;
             var suffix = null;
+            var index = 0;
+            var argumentsLength = arguments.length;
 
             if(arguments.callee.caller !== this.cat && this.wrapping){
                 this.wrapping = false;
-                if(this.myStack.length > 0){
-                    for(var i = 0; i < this.myStack.length; i += 1){
-                        prefix = this.myStack[i].prefix;
+                if(this.wrappingStack.length > 0){
+                    for(index = 0; index < this.wrappingStack.length; index += 1){
+                        prefix = this.wrappingStack[index].prefix;
                         if(prefix) this.cat(prefix);
                     }
                 } 
                 this.wrapping = true;
             }
 
-            for(var i = 0; i < arguments.length; i += 1){
-               currentArgument = arguments[i];
-
+            for(index = 0; index < argumentsLength; index += 1){
+               currentArgument = arguments[index];
+                
                 switch(typeof currentArgument){
                     case 'function' : {
                         this.buffer.push(currentArgument.call(this));
                         break;
                     }
-                    case 'string' : {
-                        this.buffer.push(currentArgument);
-                        break;
-                    }
-                    case 'number' : {
-                        this.buffer.push(currentArgument.toString());
-                        break;
-                    }
                     case 'object' : {        
-                        if(myHelper.isArray(currentArgument)){
+                        if(box.isArray(currentArgument)){
                             this.cat.apply(this, currentArgument);
                         }
+                        // else{
+                        //     this.buffer.push(currentArgument);
+                        // }
+                        break;
                     }
-                    default: break;
+                    default: 
+                        this.buffer.push(currentArgument);
+                        break;
                 }
             }
 
             if(arguments.callee.caller !== this.cat && this.wrapping){
                 this.wrapping = false;
-                if(this.myStack.length > 0){
-                    for(var i = this.myStack.length - 1; i >= 0; i -= 1){
-                        suffix = this.myStack[i].suffix;
+                if(this.wrappingStack.length > 0){
+                    for(index = this.wrappingStack.length - 1; index >= 0; index -= 1){
+                        suffix = this.wrappingStack[index].suffix;
                         if(suffix)this.cat(suffix);
                     }
                 } 
@@ -68,11 +80,12 @@
         rep : function(){
             var args = Array.slice(arguments);
             var times = args[args.length - 1];
+            var index = 0;
 
             if(typeof times === 'number'){
                 args.splice(args.length - 1, 1);
                 
-                for(var i = 0; i < times; i += 1){
+                for(var index = 0; index < times; index += 1){
                     this.cat.apply(this, args);
                 }
             } 
@@ -87,9 +100,11 @@
             var args = Array.slice(arguments);
             var condition = args[args.length - 1];
 
-            if(typeof condition === 'function') condition = condition.call(this);
+            if(typeof condition === 'function'){
+                condition = condition.call(this);
+            } 
 
-            if(typeof condition === 'boolean' && condition === true){
+            if(condition){
                 args.splice(args.length - 1, 1);
                 this.cat.apply(this, args);
             }
@@ -100,70 +115,63 @@
             return this.buffer.join("");
         },
         wrap : function(p, s){
-            this.myStack.push({prefix:p, suffix:s});
+            this.wrappingStack.push({prefix:p, suffix:s});
             this.wrapping = true;
             return this;
         },
         end: function(deep){
 
             if(deep && typeof(deep) === 'number'){
-                for(var i = 0; i < deep; i += 1){
-                    try{
-                         this.myStack.pop();
-                    }
-                    catch(err){
-                        this.wrapping = false;
-                        break;
-                    }    
-                }
+
+                this.wrappingStack.splice(this.wrappingStack.length - deep, deep);
             }
             else{
-                this.myStack.pop();
-                if(this.myStack.length <= 0){
-                    this.wrapping = false;
-                } 
-                else {
-                    this.wrapping = true;
-                }
+                this.wrappingStack.pop();
             }
 
-            if(this.myStackBackup.length > 0) {
-                this.myStack = this.myStackBackup.slice();
-                this.myStackBackup = [];
+            this.wrapping = this.wrappingStack.length > 0;
+
+            if(this.wrappingStackBackup.length > 0) {
+                this.wrappingStack = this.wrappingStackBackup;
+                this.wrappingStackBackup = [];
             }
 
             return this;
         },
         prefix : function(p){
-            this.myStack.push({prefix:p});
+            this.wrappingStack.push({prefix:p});
             this.wrapping = true;
             return this;
         },
         suffix : function(s){
-            this.myStack.push({suffix:s});
+            this.wrappingStack.push({suffix:s});
             this.wrapping = true;
             return this;
         },
         each : function(args, callBack){
+            var index = 0;
+            var argsLength = args.length;
 
-            for(var i = 0; i < args.length; i += 1){
-                callBack.call(this, args[i], i);
+            for(var index = 0; index < argsLength; index += 1){
+                callBack.call(this, args[index], index);
             }
 
             return this;
 
         },
         suspend : function(){
-            this.myStackBackup = this.myStack.slice(0);
-            this.myStack = [];
+            this.wrappingStackBackup = this.wrappingStack;
+            this.wrappingStack = [];
             return this;
         },
         when : function(condition, thenArgs, otherwiseArgs){
             var realArgs = null;
 
-            if(typeof condition === 'function') condition = condition.call(this);
+            if(typeof condition === 'function'){
+                condition = condition.call(this);
+            } 
 
-            realArgs = condition === true ? thenArgs : otherwiseArgs;
+            realArgs = condition ? thenArgs : otherwiseArgs;
             this.cat.call(this, realArgs);
 
             return this;
@@ -171,4 +179,5 @@
     }
 
     box.StringBuilder = StringBuilder;
+    box.isArray = isArray;
 })(this);
